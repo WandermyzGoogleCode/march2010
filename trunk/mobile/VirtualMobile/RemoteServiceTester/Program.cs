881d386e994be5f04f651ce7efda3207f0b693a9
lywander <lywander@de35fbbb-95e4-9a8c-2115-6ada3124d510>
2010-05-26 16:01:44 +0000

    RemoteService基本完成

---
+++ trunk/mobile/VirtualMobile/RemoteServiceTester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RemoteService;
+
+namespace RemoteServiceTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerCaller caller = new ServerCaller(errorCallback);
+            bool result = caller.EncryptedRegister(Encoding.UTF8.GetBytes("今天心情"), Encoding.UTF8.GetBytes("很好!"));
+            Console.WriteLine(result);
+        }
+
+        static void errorCallback(string errMsg, Exception e)
+        {
+            Console.WriteLine(errMsg);
+        }
+    }
+}
